7d5ced0e763650819d0f4e3ad2ffcc77875de789
AsifNinjaCoder <hossainasif502@.com>
2021-10-09 17:55:42 +0530

    first commit

---
+++ ex40.py
@@ -0,0 +1,29 @@
+class Song(object):
+
+    def __init__(self,lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+    def Something_else(self):
+        print("you are done.")        
+
+happy_bday = Song(["Happy birthday to you",
+                    "I dont want to get sued",
+                    "So I'll stop right there"])
+
+bulls_on_prade = Song(["They really around the family",
+                        "with pockets full of shells"])
+
+another_song = Song(["here your another song...."])                        
+
+happy_bday.sing_me_a_song()
+
+bulls_on_prade.sing_me_a_song()
+
+another_song.sing_me_a_song()
+
+another_song.Something_else()
+
